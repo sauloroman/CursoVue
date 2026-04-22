bda366f01cc780c9aca6addacb4c1d40b6fc3cbb
Saulo Román Santillán Nava <romansantillan1998@outlook.com>
2026-04-22 11:46:55 -0600

    feat(learning) 📕: add Vue directives examples

---
+++ 01-my-first-project/src/components/App1.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1 class="font-bold text-2xl">{{ name.toUpperCase() }}</h1>
+    <!-- <h2 v-bind:style="styleColor">Soy Azul</h2> -->
+    <h2 :style="`color: ${arrayColors[0]}`">Soy Azul</h2>
+
+    <p>{{ arrayColors }}</p>
+    <p>{{ activo ? 'Activo' : 'Inactivo' }}</p>
+
+    <p v-if="activo">Estoy Activo</p>
+    <p v-else-if="activo === false">Estoy Inactivo</p>
+    <p v-else>Estoy Indeciso</p>
+
+    <p v-if="activo">
+        <span>Icono</span>
+        Estoy activo
+    </p>
+
+    <h2 v-show="activo">Estoy activo</h2>
+</template>
+
+<script lang="ts" setup>
+const name = 'Roman';
+const styleColor = 'color: blue;';
+const arrayColors = ['blue', 'red', 'green'];
+const activo = false;
+</script>
+
+<style scoped>
+/* h1 {
+  font-weight: bold;
+  font-size: 3.2rem;
+} */
+</style>
---
+++ 01-my-first-project/src/components/App3.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul>
+        <li
+            :key="id"
+            v-for="{ name, price, description } in arrayFrutas"
+        >
+            <div class="mb-5">
+                <p>{{ name }}</p>
+                <span>{{ price }}</span>
+                <p>{{ description }}</p>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+const arrayFrutas = [
+    {
+        name: 'Manzana',
+        price: '$1.00',
+        description: 'Una manzana',
+        id: 1,
+    },
+    {
+        name: 'Pera',
+        price: '$2.00',
+        description: 'Una pera',
+        id: 2,
+    },
+    {
+        name: 'Naranja',
+        price: '$3.00',
+        description: 'Una naranja',
+        id: 3,
+    },
+];
+</script>
